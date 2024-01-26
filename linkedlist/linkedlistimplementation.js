278e44c788e1db09545c379a63686e6e6157a7c5
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-01-27 00:08:51 +0530

    Update linkedlistimplementation.js
    this is the linkedlist implementation in javascript

--- linkedlist/linkedlistimplementation.js
+++ linkedlist/linkedlistimplementation.js
@@ -1,3 +1,11 @@
+/*
+linked list is keep multiple stacks at different location in memory and point the location using node next
+first node has head before it
+last node has tail after it
+head ((data)(next))
+we can move only from left to right only
+*/
+
 // linked list in javascript
 
 // create a node for linked list
@@ -6,7 +14,7 @@ class Node{
     constructor(data) {
         // we are assigning the data to this node
         this.data = data;
-        // we are assigning the tail or next to be null 
+        // we are assigning the tail or next to be null by default
         this.next = null
     }
 }
@@ -14,7 +22,7 @@ class Node{
 // create a linkedlist class
 class LinkedList{
     constructor(){
-        // when we create a linked list we don't have the head initially
+        // when we create a linked list we don't have any node initially so we are assigning head to null
         this.head = null;
     }
     
@@ -22,9 +30,10 @@ class LinkedList{
     addFirst(data){
         // we are creating a new node instance
         const newNode = new Node(data) 
-            // we are referencing the head of the linked list
+            // we are pointing the next to the linked list head first
+            // basically we are pointing the newnode to the linked list's head 
           newNode.next = this.head;
-          // we are changing the head to the current node's tail/next
+          // we are making this node the head
           this.head = newNode;
     }
 // adding a new node at the last
@@ -32,11 +41,15 @@ class LinkedList{
         const newNode = new Node(data);
         // if the linked list in empty then the head is null so we're adding at the last but it becomes first
         if(!this.head) {
+            // we are making this new node the head of the linked list
             this.head = newNode;
             return 
         }
         // create a current variable and traverse through the linkedlist till you find the node which has next as null
+        
+        // we are taking current as the first element
         let current = this.head;
+        // this loop continues till the node.next is not null
         while(current.next){
             current = current.next;
         }
@@ -46,9 +59,11 @@ class LinkedList{
 
 //creating a size function to get the size of linked list
 size() {
+    // initializing the count variable
     let count = 0;
+    // traversing through the linked list
     let current = this.head;
-    while(current.next) {
+    while(current) {
         count++;
         current = current.next;
     }
@@ -65,6 +80,7 @@ addAt(index, data) {
     // create a new node
     const newNode = new Node(data);
     if(index === 0) {
+        // this is adding newnode at the first
         newNode.next = this.head;
         this.head = newNode;
         return;
@@ -72,36 +88,43 @@ addAt(index, data) {
     
     // traverse through the linked list
     let current = this.head;
+    // we are traversing before the index chosen to be inserted
     for(let i=0;i< index - 1;i++) {
         current = current.next
     }
+    // this will point to the node after the index
     newNode.next = current.next;
+    // the current node will become the newnode
     current.next = newNode;
 }
 
 //remove a node at the top
 removeTop(){
+    // if head is null return
     if(!this.head){
         return;
     }
-    
+    // we are placing the head to the next node
     this.head = this.head.next
 }
 
 //remove the last index of the linked list
-
 removeLast(){
     if(!this.head){
         return;
     }
     
-    let current = this.head;
+    let current = this.head; 
+    // this will stop before the last element
+    // if we use current.next it will stop at last index
     while(current.next.next){
         current = current.next;
     }
+    // we are making the next reference null
     current.next = null;
 }
 
+// remove an element from a specified index
 removeAt(index){
     if(index < 0 || index > this.size()){
         console.error("Invalid Index");
@@ -117,6 +140,7 @@ removeAt(index){
         current = current.next;
     }
     if(current.next){
+        // we are pointing the next pointer to the 2nd element from the index so we are losing reference for the index and thus the element is removed from the liked list
         current.next = current.next.next;
     }
 }
@@ -146,5 +170,6 @@ linkedlist.removeTop();
 
 linkedlist.addFirst(8);
 
+linkedlist.addAt(2,7);
 
 linkedlist.print();

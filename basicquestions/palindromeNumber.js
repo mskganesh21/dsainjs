bbaa9e2266267a262e1bd8e6ce4d1968e9c458ee
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-01-27 00:43:14 +0530

    Update palindromeNumber.js

--- basicquestions/palindromeNumber.js
+++ basicquestions/palindromeNumber.js
@@ -1,2 +1,11 @@
 // palindrome number
 // an integer number is a palindrome number if it reads same forward and backward
+
+// brute force way
+const isPalindrome = function (x){
+    return x === +x.toString().split("").reverse().join("")
+}
+
+const res = isPalindrome(10121211);
+
+console.log(res);

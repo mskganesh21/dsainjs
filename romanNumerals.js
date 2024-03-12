3086d9f9b5205d3ea5df42b53907c88438ec10bb
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-03-12 09:48:14 +0530

    Create romanNumerals.js

---
+++ romanNumerals.js
@@ -0,0 +1,30 @@
+/*
+*/
+
+const romanNumeralsArray = [{
+  number: 1000, roman: 'M'
+},{
+  number: 900, roman: 'CM'
+},{
+  number: 500, roman: 'D'
+},{
+  number: 400, roman: 'CD'
+},{
+  number: 100, roman: 'C'
+},{
+  number: 90, roman: 'XC'
+},{
+  number: 50, roman: 'L'
+},{
+  number: 40, roman: 'XL'
+},{
+  number: 10, roman: 'X'
+},{
+  number: 9, roman: 'IX'
+},{
+  number: 5, roman: 'V'
+},{
+  number: 4, roman: 'IV'
+},{
+  number: 1, roman: 'I'
+},];

71eb4c6698b6ea17c2fea9d1882692a9d9124419
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-03-19 10:09:59 +0530

    Create dsapatterns.js

---
+++ dsapatterns.js
@@ -0,0 +1,22 @@
+/*
+sliding window
+islands (matrix traversal)
+two pointers
+fast and slow pointers
+merge intervals
+cyclic sort
+in-place reversal of a linked list
+tree-breadth-first search
+tree-depth-first-search
+two heaps
+subsets
+modified binary search
+bitwise XOR
+top 'k' elements
+k-way merge
+topological sort
+0/1 knapsack
+fibonaaci numbers
+palindromic subsequence
+longest common substring
+*/

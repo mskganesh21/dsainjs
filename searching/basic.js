e7521380864c851c9b9e165387a2784d66908076
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-03-20 16:13:19 +0530

    Update basic.js

--- searching/basic.js
+++ searching/basic.js
@@ -38,8 +38,31 @@ const result1 = GlobalLinerSearch([4,5,6,7,7,0,1,2], 7);
 console.log(result1);
 
 
+/*
+BINARY SEARCH ALGORITHM
+in order for binary search to work the array has to be sorted
+o(log n)
+*/
 
+function binarySearch(nums,target){
+  let s = 0;
+  let e = nums.length -1;
+  
+  while(s<=e){
+    let m = Math.floor((s+e) / 2);
+    if(nums[m] === target) {
+      return m
+    } else if (nums[m] < target) {
+      s = m +1;
+    } else {
+      e = m-1;
+    }
+  }
+  
+  return -1
+}
 
-
+const result2 = binarySearch([-1,0,3,5,9,12], 9);
+console.log(result2)
 
 

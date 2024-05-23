a05ce47d50dd4e56f065ecdd78f375d018038893
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-05-23 11:30:48 +0530

    Update leftmostrepeatingcharacter.js

--- strings/leftmostrepeatingcharacter.js
+++ strings/leftmostrepeatingcharacter.js
@@ -20,3 +20,27 @@ const LeftMostRepeatingCharacter = (str) => {
 
 const result = LeftMostRepeatingCharacter("abcdefcdef");
 console.log(result)
+
+
+/*
+better approach
+*/
+
+const LeftMostRepeatingCharacter = (str) => {
+  let n = str.length;
+  let count = new Array(256).fill(0);
+  for(let i = 0; i < n; i++) {
+    count[str.charCodeAt(i)]++;
+  }
+  
+  for(let i = 0; i < n; i++) {
+    if(count[str.charCodeAt(i)] > 1) {
+      return str.charAt(i);
+    }
+  }
+  
+  return -1;
+}
+
+const result = LeftMostRepeatingCharacter("abcd");
+console.log(result);

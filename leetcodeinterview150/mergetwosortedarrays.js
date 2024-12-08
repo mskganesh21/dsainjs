5822b621bb384a94de5cae99f6f23eb7cbd5437b
Sai krishna Ganesh <157506559+mskganesh21@users.noreply.github.com>
2024-12-08 15:58:50 +0530

    Update mergetwosortedarrays.js

--- leetcodeinterview150/mergetwosortedarrays.js
+++ leetcodeinterview150/mergetwosortedarrays.js
@@ -1,3 +1,24 @@
+/*
+merge two sorted arrays
+
+1. create a new extra array
+2. put the elements from array 1 and array 2
+this is O(n) tc and O(n) sc
+
+
+we can use 3 pointers technique
+1. keep 1 pointer at index m
+2. keep 1 pointer at index n
+3. keep 1 pointer at the real value of m-n
+4. compare the values in both arrays and
+put the highest value at the end of
+the array
+continue this till the pointers index in 
+both arrays are valid
+5. fill nums1 with leftover elements in nums2
+
+*/
+
 var merge = function(nums1, m, nums2, n) {
    //code using 3 pointers technique
 
